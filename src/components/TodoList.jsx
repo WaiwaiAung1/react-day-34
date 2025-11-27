9698da94671fbe2ae877f09eab27d8e67445085e
Wai Wai Aung <waiwai56521@gmail.com>
2025-11-27 18:30:20 +0900

    feat:AddTodo,TodoList,TodoItem (finish)

---
+++ src/components/TodoList.jsx
@@ -0,0 +1,16 @@
+import React, { useContext } from "react";
+import TodoItem from "./TodoItem";
+import { TodoContext } from "../contexts/TodoContext";
+
+export default function TodoList() {
+  const { todos } = useContext(TodoContext);
+
+  return (
+    <ul>
+      {todos.map((todo) => (
+        <TodoItem key={todo.id} todo={todo} />
+      ))}
+    </ul>
+  );
+}
+
